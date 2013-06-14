064e1409c98cd38b5bdb204b86c6d51d8970454a
Kevin Bjorke <kevin.bjorke@ingtec.com>
2013-06-14 16:39:35 -0700

    open_me_to_install.ma is now smarter about redundant installations

--- Python/VisualizerToolsSetup.py
+++ Python/VisualizerToolsSetup.py
@@ -1,6 +1,11 @@
 """
 Contains the same script (except for the invovcation) as found as a script node
    in the "open_me_to_install.ma" scene.
+   
+   TO-DO: check existing paths -- don't do anything if they're already set!
+   Look for updates on git
+   Can there be both userSetup.mel and userSetup.py?
+   What about maya/scripts instead of maya/version/scripts?
 """
 
 import os, sys, re, maya
@@ -8,23 +13,42 @@ import os, sys, re, maya
 def MayaVisToolSetup():
   sceneName = maya.cmds.file(sn=True,q=True)
   visToolsDir = os.path.split(sceneName)[0]
-  # print 'These tools have been placed in "%s"' % (visToolsDir)
-  # sys.path.append(os.path.join(visToolsDir,'Python'))
+  visMelPath = visToolsDir+'/MEL'
+  visPyPath = os.path.join(os.path.normpath(visToolsDir),'Python')
   what = "MayaVisualizerTools"
   mayaVers = os.path.split(os.environ['MAYA_LOCATION'])[-1].__str__()[4:]
   x64 = os.environ.get('PROCESSOR_ARCHITECTURE','32bit')
   if x64 == 'AMD64':
     mayaVers += '-x64'
   userSetupFile = None
+  userPythonFile = None
+  needMel = True
+  needPy = True
   previous = '// end //\n'
-  for pName in maya.mel.eval('getenv "MAYA_SCRIPT_PATH"').split(';'):
+  for pName in sys.path:
+    if pName == visPyPath:
+      print 'Python path already includes "%s"'%(visPyPath)
+      needPy = False
+  existingMelPath = maya.mel.eval('getenv "MAYA_SCRIPT_PATH"')
+  for pName in existingMelPath.split(';'):
+    if pName == visMelPath:
+      print 'Mel path already includes "%s"'%(visMelPath)
+      needMel = False
     if os.path.exists(pName):
-      print "check "+pName
       for fName in os.listdir(pName):
-        if fName == 'userSetup.mel':
+        if needMel and fName == 'userSetup.mel':
           userSetupFile = os.path.join(pName,fName)
+        if needPy and fName == 'userSetup.py':
+          userPythonFile = os.path.join(pName,fName)
+  if not (needMel or needPy):
+    maya.cmds.confirmDialog(title=what+' install', message='Already Installed!\nNo need for changes.',button='OK')
+    return
   if userSetupFile:
     fp = open(userSetupFile,'r')
+    previous = fp.read()
+    fp.close()
+  elif userPythonFile:
+    fp = open(userPythonFile,'r')
     previous = fp.read()
     fp.close()
   else:
@@ -33,30 +57,41 @@ def MayaVisToolSetup():
     if not os.path.exists(mayaDefaultScriptDir):
       maya.cmds.confirmDialog(title=what+' install failure',message='Cannot find location to create userSetup.mel\n%s'%(mayaDefaultScriptDir),button='OK')
       return
-    userSetupFile = os.path.join(mayaDefaultScriptDir,'userSetup.mel')
+    userPythonFile = os.path.join(mayaDefaultScriptDir,'userSetup.py')
+    userSetupFile = userPythonFile
+    # userSetupFile = os.path.join(mayaDefaultScriptDir,'userSetup.mel')
     print 'Will create "%s"'%(userSetupFile)
   print 'Adding %s paths to "%s"'%(what,userSetupFile)
   fp = open(userSetupFile,'w')
   hasVisTools = False
-  melPath = visToolsDir+'/MEL'
-  pyPath = os.path.join(os.path.normpath(visToolsDir),'Python')
-  pyPath = re.sub(r'\\', r'\\\\', pyPath)
+  hdr = '// '
+  if userPythonFile:
+    visPyPath = re.sub(r'\\', r'\\\\', visPyPath)
+    hdr = '## '
   for line in previous.split('\n'):
-    isDelim = line[:len(what)+3] == '// ' + what
+    isDelim = line[:len(what)+3] == hdr + what
     if isDelim:
       hasVisTools = True
-    if not hasVisTools:
-      fp.write('// '+what+' '+'='*20+'\n\n')
-      fp.write('putenv "{0}" ( `getenv "{0}"` + \":{1}\");\n'.format('MAYA_SCRIPT_PATH',melPath))
+    if not hasVisTools and not userPythonFile:
+      # that is, we insert into userSetup.mel as early as possible
+      fp.write(hdr+what+' '+'='*20+'\n\n')
+      fp.write('putenv "{0}" ( `getenv "{0}"` + \":{1}\");\n'.format('MAYA_SCRIPT_PATH',visMelPath))
       fp.write('python("import sys");\n')
-      fp.write(r'python("sys.path.append(r\"%s\")");'%(pyPath)+'\n')
-      fp.write('// '+what+' '+'='*20+'\n')
+      fp.write(r'python("sys.path.append(r\"%s\")");'%(visPyPath)+'\n')
+      fp.write(hdr+what+' '+'='*20+'\n')
       hasVisTools = True
+    # we insert into the userSetup.py file as LATE as possible
+    if userPythonFile and not hasVisTools:
+      fp.write(hdr+what+' '+'='*20+'\n')
+      fp.write("import sys, maya\n")
+      fp.write('sys.path.append(r"%s")'%(visPyPath))
+      fp.write('maya.mel(\'putenv "{0}" ( `getenv "{0}"` + \";{1}\");\')\n'.format('MAYA_SCRIPT_PATH',visMelPath))
+      fp.write(hdr+what+' '+'='*20+'\n')
     fp.write(line+'\n')
   fp.close()
   maya.cmds.confirmDialog(title=what+' install', message='Paths assigned for Maya %s!\nNow just restart Maya'%(mayaVers),button='OK')
 
-# the script node also calls the above function:
 # MayaVisToolSetup()
 
 ########### eof ##
+
